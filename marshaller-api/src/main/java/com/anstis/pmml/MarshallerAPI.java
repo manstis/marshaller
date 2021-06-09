1e91c7b0804e9b63946579cb1bd1942078c33340
Michael Anstis <manstis@redhat.com>
2021-06-09 16:09:59 +0100

    Add serialisation from webapp to highlight issues.

--- marshaller-api/src/main/java/com/anstis/pmml/MarshallerAPI.java
+++ marshaller-api/src/main/java/com/anstis/pmml/MarshallerAPI.java
@@ -6,6 +6,6 @@ public class MarshallerAPI implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        int a=0;
+
     }
 }
--- marshaller-client/src/main/java/com/anstis/pmml/App.java
+++ marshaller-client/src/main/java/com/anstis/pmml/App.java
@@ -1,7 +1,9 @@
 package com.anstis.pmml;
 
+import com.anstis.pmml.model.Header;
 import com.anstis.pmml.model.PMML;
 import com.anstis.pmml.model.PMML_XMLMapperImpl;
+import com.anstis.pmml.model.Scorecard;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
@@ -19,8 +21,34 @@ public class App implements EntryPoint {
         loadXMLButton.addClickHandler(clickEvent -> {
             final PMML_XMLMapperImpl mapper = new PMML_XMLMapperImpl();
             try {
+                // See https://github.com/treblereel/mapper-xml/issues/84
+                // XmlElementRefs are no correctly deserialised
                 final PMML pmml = mapper.read(Resources.INSTANCE.xml().getText());
                 Window.alert(pmml.getHeader().getDescription());
+            } catch (Exception e) {
+                Window.alert(e.getMessage());
+            }
+        });
+
+        final Button saveXMLButton = new Button("Save as XML");
+        RootPanel.get("saveXMLButtonContainer").add(saveXMLButton);
+        saveXMLButton.addClickHandler(clickEvent -> {
+            final PMML_XMLMapperImpl mapper = new PMML_XMLMapperImpl();
+            try {
+                // See https://github.com/treblereel/mapper-xml/issues/84
+                // XmlElementRefs are no correctly serialised
+                final PMML pmml = new PMML();
+                final Header header = new Header();
+                header.setDescription("Test description");
+                header.setCopyright("Test copyright");
+                header.setModelVersion("Test");
+                pmml.setHeader(header);
+
+                final Scorecard scorecard = new Scorecard();
+                pmml.getModel().add(scorecard);
+
+                final String xml = mapper.write(pmml);
+                Window.alert(xml);
             } catch (Exception e) {
                 Window.alert(e.getMessage());
             }
